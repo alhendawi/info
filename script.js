b2ef3f862761b995a762bcbcf701b06149a07bf2
alhendawi <gameromke@gmail.com>
2024-12-24 21:25:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.querySelector('.nav-toggle');
     const navLinks = document.querySelector('.nav-links');
+    const navOverlay = document.createElement('div');
+    navOverlay.classList.add('nav-overlay');
+    document.body.appendChild(navOverlay);
 
     if (navToggle && navLinks) {
         navToggle.addEventListener('click', () => {
             navLinks.classList.toggle('show');
+            navOverlay.classList.toggle('show');
+            // Toggle hamburger icon to cross shape
+            navToggle.classList.toggle('active');
+        });
+
+        // Close the menu if overlay is clicked
+        navOverlay.addEventListener('click', () => {
+            navLinks.classList.remove('show');
+            navOverlay.classList.remove('show');
+            navToggle.classList.remove('active');
         });
     }
 });
+
